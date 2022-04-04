91aef9dddc1a98b43333c0710a6690f5e7f9aabe
aiqiqi521 <aiqiqi521@petalmail.com>
2022-04-04 14:45:22 +0800

    update ingore

--- learn_t/src/main.rs
+++ learn_t/src/main.rs
@@ -1,15 +1,7 @@
+
 fn main() {
-    println!("Hello, world!");
-    println!("Hello, world, from rust PC");
-    aiqiqi521_says();
-
-    aiqiqi521_says2();
+    let mut v = Vec::new();
+    v.push(1);
+    println!("{:?}", v);
 }
 
-fn aiqiqi521_says() {
-    println!("ai qiqi 521 day by day");
-}
-
-fn aiqiqi521_says2() {
-    println!("i have to say two days");
-}
